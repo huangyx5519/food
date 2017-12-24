7ce4a317cfdbc299f52eaabc416799be4085cdcf
hyx <hyx@hyxdeMBP.lan>
2017-12-25 00:01:45 +0800

    bu

---
+++ food/CommentedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentedTableViewCell.swift
+//  food
+//
+//  Created by hyx on 2017/12/24.
+//  Copyright © 2017年 hyx. All rights reserved.
+//
+
+import UIKit
+
+class CommentedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ food/SharedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SharedTableViewCell.swift
+//  food
+//
+//  Created by hyx on 2017/12/24.
+//  Copyright © 2017年 hyx. All rights reserved.
+//
+
+import UIKit
+
+class SharedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
